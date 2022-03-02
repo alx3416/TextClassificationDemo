7b9536ddab532cc513304a1eedf0aa7dcf85dafa
alx3416 <alx3416@hotmail.com>
2022-03-02 00:38:36 -0600

    testing values for network

--- exploring_data.py
+++ exploring_data.py
@@ -1,13 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+# for preprocessing
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from sklearn.utils import class_weight
+import numpy as np
+from keras.utils import to_categorical
+# for LSTM
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import SpatialDropout1D, LSTM, Dense, Embedding, Bidirectional
+from keras.callbacks import EarlyStopping
+from keras import Sequential, Model, Input
+# for reporting
+from sklearn.metrics import classification_report, confusion_matrix
 
 trainData = pd.read_csv("data/customer-issues-train.csv")
 listColumnNames = list(trainData.columns)
 print('List of column names : ', listColumnNames)
 productIndex = 1
+consumerMessageIndex = 5
 productColumnName = listColumnNames[productIndex]
+consumerMessageColumnName = listColumnNames[consumerMessageIndex]
 productTypes = trainData[productColumnName].value_counts()
 
+for row in range(9):
+    print(trainData.loc[row].at[consumerMessageColumnName])
 
 plt.bar(productTypes.index, productTypes.values)
 plt.xticks(rotation='vertical')
@@ -15,3 +35,105 @@ plt.title('Dataset classes')
 plt.tight_layout()
 plt.savefig('plots/dataset_classes.png')
 plt.show()
+
+# preprocessing consumer mesages
+df = trainData.reset_index(drop=True)
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+
+def clean_text(text):
+    """
+        text: a string
+
+        return: modified initial string
+    """
+    text = text.lower()  # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text)  # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+    text = BAD_SYMBOLS_RE.sub('', text)  # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing.
+    text = text.replace('x', '')
+    #    text = re.sub(r'\W+', '', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)  # remove stopwors from text
+    return text
+
+
+trainData[consumerMessageColumnName] = trainData[consumerMessageColumnName].apply(clean_text)
+trainData[consumerMessageColumnName] = trainData[consumerMessageColumnName].str.replace('\d+', '')
+
+for row in range(9):
+    print(trainData.loc[row].at[consumerMessageColumnName])
+
+# LSTM Modelling
+# The maximum number of words to be used. (most frequent)
+MAX_NB_WORDS = 250
+# Max number of words in each complaint.
+MAX_SEQUENCE_LENGTH = 50
+# This is fixed.
+EMBEDDING_DIM = 30
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+tokenizer.fit_on_texts(trainData[consumerMessageColumnName].values)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+# truncate and pad all same length for modelling
+inputData = tokenizer.texts_to_sequences(trainData[consumerMessageColumnName].values)
+inputData = pad_sequences(inputData, maxlen=MAX_SEQUENCE_LENGTH)
+print('Shape of data tensor:', inputData.shape)
+
+# getting numbers as outputs for trainning
+outputLabels = pd.get_dummies(trainData[productColumnName]).values
+print('Shape of label tensor:', outputLabels.shape)
+
+
+# class weights balancing dataset
+def getLabelsArray(outputLabels):
+    labelsArray = np.zeros(outputLabels.shape[0])
+    for row in range(outputLabels.shape[0]):
+        labelsArray[row] = np.argmax(outputLabels[row])
+    return labelsArray
+
+
+outputArray = getLabelsArray(outputLabels)
+# categorical KERAS
+categorical_labels = to_categorical(outputArray, num_classes=12)
+class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(outputArray), y=outputArray)
+
+# Embed each integer in a 128-dimensional vector
+model = Sequential()
+model.add(Embedding(input_dim=MAX_NB_WORDS, output_dim=EMBEDDING_DIM, input_length=MAX_SEQUENCE_LENGTH))
+# Add 2 bidirectional LSTMs
+model.add(Bidirectional(LSTM(6, dropout=0.4, recurrent_dropout=0.4, return_sequences=True)))
+model.add(Bidirectional(LSTM(6, dropout=0.4, recurrent_dropout=0.4)))
+# Add a classifier
+model.add(Dense(12, activation="sigmoid"))
+model.summary()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+
+epochs = 3
+batch_size = 4096
+history = model.fit(inputData, categorical_labels, epochs=epochs, batch_size=batch_size, validation_split=0.15, class_weight=class_weights)
+
+# Reporting and quality metrics
+score = model.evaluate(inputData, outputLabels, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+y_pred = model.predict(inputData)
+y_pred = np.argmax(y_pred, axis=1)
+y_test = outputArray
+target_names = list(productTypes.index)
+print('Confusion Matrix')
+print(confusion_matrix(y_test, y_pred))
+print('Classification Report')
+#target_names = ['0', '1', '2','3','4','5','6','7','8','9']
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+# saving
+model.save('output/RNN_Bidirectional_balanced.h5')
+hist_df = pd.DataFrame(history.history)
+hist_csv_file = 'output/history.csv'
+with open(hist_csv_file, mode='w') as f:
+    hist_df.to_csv(f)
+
---
+++ preprocessing.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.datasets import imdb
+from keras.layers import LSTM, Embedding, Dense
+from keras.preprocessing.sequence import pad_sequences
+from keras import Sequential
+# fix random seed for reproducibility
+np.random.seed(7)
+# load the dataset but only keep the top 6000 words
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=6000)
+# pad input sequences
+X_train = pad_sequences(X_train, maxlen=500)
+X_test = pad_sequences(X_test, maxlen=500)
+#model
+model = Sequential()
+model.add(Embedding(6000, 32, input_length=500))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
